a1b8c7b47dea83ecadd08637990d1b4be02104be
welloliveiraoficial-cmyk <welloliveiraoficial@gmail.com>
2026-09-15 03:15:28 -0300

    Update verificar-atrasos.js

--- api/verificar-atrasos.js
+++ api/verificar-atrasos.js
@@ -26,7 +26,7 @@ const db = admin.firestore();
 
 const HORARIOS = [
   "08:00", "09:00", "10:00", "12:00", "16:00",
-  "20:00", "21:00", "22:00", "00:00", "19:40"
+  "20:00", "21:00", "22:00", "00:00"
 ];
 
 const MEDICAMENTOS = {
@@ -38,8 +38,7 @@ const MEDICAMENTOS = {
   "20:00": ["Atorvastatina"],
   "21:00": ["Losartana", "Quetiapina"],
   "22:00": ["Clonazepam"],
-  "00:00": ["Levetiracetam"],
-  "19:40": ["Teste"]
+  "00:00": ["Levetiracetam"]
 };
 
 const MINUTOS_TOLERANCIA = 30;
